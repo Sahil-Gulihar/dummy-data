0fe350a9c318bcd3b6e942edfc07562787789c33
Sahil Gulihar <sahilgullihar@gmail.com>
2025-08-12 13:21:36 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <meta charSet="UTF-8" />
+      <meta name="description" content="Dummy data app for testing purposes" />
+      <meta name="keywords" content="dummy data, test app, react, sample, SEO, web crawler" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <h1> THIS IS DUMMY DATA</h1>
+      <p> This is some dummy data for testing purposes.</p>
+      <ul>
+      <li>Dummy Item 1</li>
+      <li>Dummy Item 2</li>
+      <li>Dummy Item 3</li>
+      </ul>
+    </>
+    );
+}
+<header>
+  <h2>Header Section</h2>
+</header>
+
+export default App
